27fb4de24cd62f543af29708f80147ccd60836db
Maxwell <Maxwell.Keely@epa.gov>
2016-09-15 14:59:10 -0400

    KMgraph6-3

--- lesson-6-3.R
+++ lesson-6-3.R
@@ -10,15 +10,25 @@ surveys <- read.csv("data/surveys.csv", na.strings = "", stringsAsFactors = FALS
 in1 <- selectInput("pick_species",
                    label = "Pick a species",
                    choices = unique(species[["species_id"]]))
-...
-tab <- tabPanel("Species", in1, ...)
+out2 <- plotOutput("species_plot")
+out3<-  textOutput("species_name")
+tab <- tabPanel("Species", in1, out2, out3)
 ui <- navbarPage(title = "Portal Project", tab)
 
 # Server
 server <- function(input, output) {
   output[["species_plot"]] <- renderPlot(
-    ...
+    surveys %>%
+    filter(species_id== input[["pick_species"]]) %>% 
+      ggplot(aes(year))+ 
+      geom_bar()
   )
+  output[["species_name"]] <- renderText(
+    species %>%
+      filter(species_id== input[["pick_species"]]) %>%
+      select(genus, species) %>%
+      paste(collapse=' ')
+    )
 }
 
 # Create the Shiny App
